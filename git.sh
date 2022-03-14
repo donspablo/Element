918923e610db48c75fb63d13c6c92e93cce1643c
don Pablo <don@obeyi.com>
2022-03-14 10:06:22 +0200

    update

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo git add .  || git commit -m "update"
+sudo git commit -m "update" || git commit -m "update" -a
+sudo git pull || git push
+sudo git push || git pull
+sudo git add .    || git pull
+sudo git pull || git add .
+sudo git pull || git commit -m "update" -a
+sudo git add . || git commit -m "update" -a
+sudo git commit -m "update" -a  || git add .
